24c987bb55f667a3c78cb9f68aadc1d9d158a54e
sean <chnlds@gmail.com>
2017-03-23 09:22:22 +0800

    add publish time filter

--- crawler.py
+++ crawler.py
@@ -1,27 +1,56 @@
+import io
 import requests
 from bs4 import BeautifulSoup
-import json
-import traceback
-import threading
-import time
-import io
+import html2text
+from datetime import datetime
+
+from selenium import webdriver
+# https://pypi.python.org/pypi/selenium/2.7.0
+
+
 
 def crawl_moneydigest():
     # Crawl from http://www.moneydigest.sg/category/deals/dining/page/6/
-    num_pages = 27
+    num_pages = 31
     base_url = "http://www.moneydigest.sg/category/deals/dining/page/{}/"
+    new_url = "http://www.moneydigest.sg/category/deals/dining/"
     count = 0
     with io.open('result.md', 'w', encoding='utf-8') as f:
         for i in range(num_pages):
             url = base_url.format(i+1)
+
+            # broswer = webdriver.Safari()
+            # broswer.get(url)
+            # html = broswer.page_source
+            # print html
+            # break
+
+            ####
             p = requests.get(url)
+            # p = requests.get(new_url)
+            print 'page %s' % (i+1)
             soup = BeautifulSoup(p.content, "html.parser")
+            # print soup
+            # soup = BeautifulSoup(p.content, "html5lib")
             soup.encode("utf-8")
+            # html = html2text.html2text(repr(soup))
+            # print html
+            # print soup
+
             articles = soup.find_all("article")
 
             for art in articles:
                 try:
                     count+=1
+                    print 'item %s' % count
+                    times = art.header.find('span',{'class':'posted-on'}).find_all('time')
+                    publish_time = times[0].text
+                    print publish_time
+                    f.write(u'publish time {}\n'.format(publish_time))
+                    pt = datetime.strptime('March 22, 2017', '%B %d, %Y')
+                    if pt < datetime(2017, 2, 1):
+                        print 'Old post, older than Feb 1, 2017'
+                        continue
                     header = art.header.h2
                     title = header.a.text
                     link = header.a['href']
